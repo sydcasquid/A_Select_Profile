90be1d1dd3e56ddba4ec1ed9151cf3f0ca38bc97
sydcasquid <sacarril@purdue.edu>
2024-10-21 16:27:45 -0400

    Initial

---
+++ JavaJquery.js
@@ -0,0 +1,23 @@
+$('h4').html("<i>American/Mexican</i>");
+$("#name").text("Sydnee Carrillo");
+$("#major").text("CGT: Computer Graphics Technology - Interactive Multimedia");
+$("#year").text("Senior");
+
+
+// const pageReady = () => {
+//     $("#name").text(
+//         "Sydnee Carrillo"
+//     );
+
+//     $("#major").text( 
+//         "CGT: Computer Graphics Technology - Interactive Multimedia"
+//     );
+
+//     $("#year").text(
+//         "Senior"
+//     );
+
+//     $('h4').html("<b>American/Mexican</b>");
+// };
+
+// $(document).ready(pageReady);
